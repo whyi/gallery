54746d79d02d8bd0dfac6e36a3b33e5aa18e586e
Jeonggyu Lee <glaze@whyi.net>
2014-09-23 13:05:50 -0700

    replace ensure_inclusion_of with validate_inclusion_of

--- spec/models/art_spec.rb
+++ spec/models/art_spec.rb
@@ -11,14 +11,14 @@ describe Art do
     it { should ensure_length_of("title").is_at_least(10).is_at_most(49) }
     it { should validate_presence_of("description") }
     it { should validate_presence_of("category_cd") }
-    it { should ensure_inclusion_of("category_cd").in_range(0..2) }
+    it { should validate_inclusion_of("category_cd").in_range(0..2) }
     it { should validate_presence_of("width") }
-    it { should ensure_inclusion_of("width").in_range(10..200) }
+    it { should validate_inclusion_of("width").in_range(10..200) }
     it { should validate_presence_of("height") }
-    it { should ensure_inclusion_of("height").in_range(10..200) }
+    it { should validate_inclusion_of("height").in_range(10..200) }
     it { should validate_presence_of("filename") }
     it { should validate_presence_of("year") }
-    it { should ensure_inclusion_of("year").in_range(2010..Date.today.year) }
+    it { should validate_inclusion_of("year").in_range(2010..Date.today.year) }
     it { should validate_presence_of("medium") }
   end
 end
